f9ca3563d6d9c157bfabfba260ee6e4079946115
Martin RIBOUCHON <mribouch@e3r11p12.42.fr>
2020-06-22 18:27:35 +0200

    begin animation

--- srcs/main.c
+++ srcs/main.c
@@ -16,6 +16,7 @@
 #include <math.h>
 //test
 
+
 typedef struct	s_spritesheet
 {
 	uint32_t	*pixels;
@@ -28,6 +29,14 @@ typedef struct	s_spritesheet
 	int			sprite_line;
 	int			sprite_height;
 }				t_spritesheet;
+
+typedef struct	s_animation
+{
+	int			anim;
+	int			nb_sprite;
+	int			speed;
+	int			frame;
+}				t_animation;
 
 typedef struct	s_point
 {
@@ -133,6 +142,7 @@ float lerp(float v0, float v1, float t)
 	return (1 - t) * v0 + t * v1;
 }
 
+
 void			display_sprite(t_spritesheet *ss ,int num, uint32_t *screen, int posx, int posy)
 {
 	int	x;
@@ -140,7 +150,7 @@ void			display_sprite(t_spritesheet *ss ,int num, uint32_t *screen, int posx, in
 
 	x = 0;
 	y = 0;
-	ft_putendl("coucou je suis bien dans display_sprite");
+	// ft_putendl("coucou je suis bien dans display_sprite");
 	for (int i = 0; i < ss->sprite_h; i++)
     {
         for (int j = 0; j < ss->sprite_w; j++)
@@ -149,6 +159,30 @@ void			display_sprite(t_spritesheet *ss ,int num, uint32_t *screen, int posx, in
             	screen[(i + posy) * WIDTH + (j + posx)] =  ss->sprite[num][i * ss->sprite_w + j];
         }
     }
+}
+
+void		animate_sprite(t_animation *anim, t_spritesheet *ss, uint32_t *pixels, int x, int y)
+{
+	int	line;
+	int	index;
+
+	line = ss->sprite_line * anim->anim;
+	display_sprite(ss, line + anim->frame, pixels, x, y);
+	printf("index = %d\n", line + anim->frame);
+	anim->frame++;
+	if (anim->frame == ss->sprite_line)
+		anim->frame = 0;
+}
+
+t_animation		load_anim(int anim, int nb_sprite, int speed)
+{
+	t_animation ret;
+
+	ret.anim = anim;
+	ret.nb_sprite = nb_sprite;
+	ret.speed = speed;
+	ret.frame = 0;
+	return (ret);
 }
 
 void			display_ss(t_spritesheet *ss, uint32_t *pixels, int size)
@@ -171,7 +205,7 @@ void			display_ss(t_spritesheet *ss, uint32_t *pixels, int size)
 	// 	j = 0;
 	// 	i++;
 	// }
-	ft_putendl("coucou je suis vien dednas iuy c'est de kja lfeisse lzrdsz dze ra maez la pirz");
+	// ft_putendl("coucou je suis vien dednas iuy c'est de kja lfeisse lzrdsz dze ra maez la pirz");
 	int	x;
 	int	y;
 	x = 0;
@@ -206,8 +240,8 @@ void		grab_sprite(t_spritesheet *ss, int num)
 	starty = (num / ss->sprite_line) * ss->sprite_h;
 	// if (startx >= ss->sheet_w)
 	// 	starty = startx/ss->sheet_w;
-	printf("num = %d, spritew = %d, spriteh = %d, startx = %d, starty = %d, ssw = %d, ssh = %d\n", num, ss->sprite_w, ss->sprite_h, startx, starty,ss->sheet_w, ss->sheet_h);
-	printf("index = %d, sprite_line = %d, sprite_height = %d\n", startx + j + (i + starty) * (ss->sheet_w), ss->sprite_line, ss->sprite_height);
+	// printf("num = %d, spritew = %d, spriteh = %d, startx = %d, starty = %d, ssw = %d, ssh = %d\n", num, ss->sprite_w, ss->sprite_h, startx, starty,ss->sheet_w, ss->sheet_h);
+	// printf("index = %d, sprite_line = %d, sprite_height = %d\n", startx + j + (i + starty) * (ss->sheet_w), ss->sprite_line, ss->sprite_height);
 	while (i < ss->sprite_h)
 	{
 		while (j < ss->sprite_w)
@@ -242,7 +276,7 @@ t_spritesheet	*load_spritesheet(char *path, int nb_sprite, int line, int height)
 	ss->sheet_h = bmp_tex->head.height;
 	ss->sprite_w = bmp_tex->head.width / line;
 	ss->sprite_h = bmp_tex->head.height / height;
-	if (!(ss->sprite = malloc(sizeof(uint32_t) * (nb_sprite))))
+	if (!(ss->sprite = malloc(sizeof(uint32_t*) * (nb_sprite))))
 		(throw_null("load_spritesheet", "failed to malloc sprite"));
 	while (i < nb_sprite)
 	{
@@ -253,10 +287,8 @@ t_spritesheet	*load_spritesheet(char *path, int nb_sprite, int line, int height)
 	i = 0;
 	while (i < nb_sprite)
 	{
-		ft_putendl("avant");
 		grab_sprite(ss, i);
 		i++;
-		ft_putendl("apres");
 	}
 	return (ss);
 }
@@ -1299,6 +1331,15 @@ int main(int argc, char **argv)
 	t_bitmap_texture *cockpit = load_bmp("assets/cockpit1080.bmp");
 	t_spritesheet	*testss = load_spritesheet("sprite-sheet-png-walking-2.bmp", 36, 9, 4);
 	t_map map;
+	t_animation		walk_left;
+	t_animation		walk_right;
+	t_animation		walk_front;
+	t_animation		walk_behind;
+
+	walk_front = load_anim(0, testss->sprite_line, 0);
+	walk_left = load_anim(1, testss->sprite_line, 0);
+	walk_behind = load_anim(2, testss->sprite_line, 0);
+	walk_right = load_anim(3, testss->sprite_line, 0);
 	init_map(&map,	load_bmp("assets/maps/volcano/heightmap.bmp"),
 			load_bmp("assets/maps/volcano/colormap.bmp"),
 			"volcano");
@@ -1367,6 +1408,10 @@ int main(int argc, char **argv)
 		{
 			render(&game.screen, &map, &player, bg, cockpit);
 			display_ss(testss, game.screen.pixels, game.screen.width);
+			animate_sprite(&walk_front, testss, game.screen.pixels, 200, 500);
+			animate_sprite(&walk_left, testss, game.screen.pixels, 270, 500);
+			animate_sprite(&walk_behind, testss, game.screen.pixels, 340, 500);
+			animate_sprite(&walk_right, testss, game.screen.pixels, 410, 500);
 			// display_sprite(testss, 0, game.screen.pixels, 0, 0);
 			// display_sprite(testss, 1, game.screen.pixels, 64, 64);
 			// display_sprite(testss, 2, game.screen.pixels, 128, 128);
@@ -1374,7 +1419,7 @@ int main(int argc, char **argv)
 			// display_sprite(testss, 4, game.screen.pixels, 264, 264);
 			// display_sprite(testss, 5, game.screen.pixels, 328, 328);
 			// display_sprite(testss, 6, game.screen.pixels, 400, 400);
-			display_sprite(testss, z, game.screen.pixels, WIDTH / 2, 700);
+			// display_sprite(testss, z, game.screen.pixels, WIDTH / 2, 700);
 			// display_sprite(testss, 9, game.screen.pixels, 500, 500);
 			// display_sprite(testss, 19, game.screen.pixels, 564, 500);
 			// display_sprite(testss, 18, game.screen.pixels, 620, 500);
